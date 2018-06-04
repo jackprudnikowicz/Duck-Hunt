12e75381ee432b2a7a31aff9b58cb819a5738a20
Unknown <jackprudnikowicz@gmail.com>
2018-06-04 10:17:49 -0400

    Added temporary files

--- Code/Releases/Version 1.0/core/src/gdx/game/Main.java
+++ Code/Releases/Version 1.0/core/src/gdx/game/Main.java
@@ -2,33 +2,65 @@ package gdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import javafx.scene.layout.Background;
 
-public class Main extends ApplicationAdapter {
+public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	
+	Texture img, img2, img3;
+	Sprite sprite;
+	int nScreen=1;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		// lol
+		img = new Texture("Background.jpg");
+		img2 = new Texture("0.png");
+		img3 = new Texture("Range.jpg");
+		sprite = new Sprite(img2);
+		sprite.setPosition(0, 200);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		if(Gdx.input.isKeyPressed(Input.Keys.A)){
+			sprite.translateX(-5f);
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.D)){
+			sprite.translateX(5f);
+		}
+		if(sprite.getX()>=img.getWidth()-sprite.getWidth()/2){
+			nScreen =2;
+			sprite.setPosition(0, 200);
+		}
+		if(sprite.getX()<=-1&&nScreen==2){
+			nScreen =1;
+			sprite.setPosition(img.getWidth()-sprite.getWidth()/2, 200);
+		}
+		else if(sprite.getX()<=-1){
+			nScreen=3;
+		}
+		//Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		if(nScreen ==1){
+			batch.draw(img, 0, -95);
+		}
+		else if(nScreen ==2){
+			batch.draw(img3, 0, 0);
+		}
+		batch.draw(img2, sprite.getX(), sprite.getY());
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		img2.dispose();
 	}
 }
--- Code/Releases/Version 1.0/desktop/src/gdx/game/desktop/DesktopLauncher.java
+++ Code/Releases/Version 1.0/desktop/src/gdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,13 @@ package gdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import gdx.game.Main;
+import gdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Main(), config);
+		config.width=1000;
+		config.height=800;
+		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
