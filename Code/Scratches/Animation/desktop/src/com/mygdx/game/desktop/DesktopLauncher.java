e4335d4db20e1cf53b3f00a3e74a900c3d563ab4
Unknown <jackprudnikowicz@gmail.com>
2018-06-06 09:54:25 -0400

    version 1 of screen limits

--- Code/Scratches/Animation/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Code/Scratches/Animation/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,14 +1,12 @@
-package gdx.game.desktop;
+package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import gdx.Animation.Animation;
+import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width=1000;
-		config.height=800;
-		new LwjglApplication(new Animation(), config);
+		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
